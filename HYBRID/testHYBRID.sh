6a979082b7cd3abf05aad6afbc2138d77d2de052
John Harrigan <jharriga@redhat.com>
2025-03-25 11:42:40 -0400

    Create testHYBRID.sh

---
+++ HYBRID/testHYBRID.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start pmlogger in background - on Container
+podman exec -w /tmp/test pcp-test ./HYBRID/start_archive.sh&
+
+# Run workload - on Host
+runtime=100
+thread_cnt=$(nproc)
+
+##sysbench cpu run --time="$runtime" --threads="$thread_cnt"
+sysbench cpu run --time=30 --threads=4
+
+# Stop pmlogger - on Container
+podman exec -w /tmp/test pcp-test ./HYBRID/stop_archive.sh
+
