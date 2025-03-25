68855e961c2d5de267832770b06d74284caf081d
John Harrigan <jharriga@redhat.com>
2025-03-25 11:43:07 -0400

    Update testHYBRID.sh

--- HYBRID/testHYBRID.sh
+++ HYBRID/testHYBRID.sh
@@ -7,8 +7,8 @@ podman exec -w /tmp/test pcp-test ./HYBRID/start_archive.sh&
 runtime=100
 thread_cnt=$(nproc)
 
-##sysbench cpu run --time="$runtime" --threads="$thread_cnt"
-sysbench cpu run --time=30 --threads=4
+sysbench cpu run --time="$runtime" --threads="$thread_cnt"
+##sysbench cpu run --time=30 --threads=4
 
 # Stop pmlogger - on Container
 podman exec -w /tmp/test pcp-test ./HYBRID/stop_archive.sh
